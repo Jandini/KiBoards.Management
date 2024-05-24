bae57aba1c0954d438e50701988fee9b32a7a14d
Matt <matt.janda.kingston@gmail.com>
2024-05-24 11:06:12 +0100

    Add kibana http client

---
+++ src/KiBoards.Management/Models/Spaces/KibanaSpace.cs
@@ -0,0 +1,12 @@
+﻿namespace KiBoards.Management
+{
+    public class KibanaSpace
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public List<string> DisabledFeatures { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
