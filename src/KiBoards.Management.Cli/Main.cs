65c2f540ffeacc54336ec3457b7a3a4db5d85787
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-18 14:14:35 +0530

    Update comments and namespaces

--- src/KiBoards.Management.Cli/Main.cs
+++ src/KiBoards.Management.Cli/Main.cs
@@ -1,4 +1,5 @@
 ﻿using KiBoards.Management;
+using KiBoards.Management.Models.Spaces;
 using Microsoft.Extensions.Logging;
 
 internal class Main(ILogger<Main> logger, KibanaHttpClient kibanaHttpClient) 
@@ -7,6 +8,31 @@ internal class Main(ILogger<Main> logger, KibanaHttpClient kibanaHttpClient)
     {
         logger.LogInformation("Making kibana dark");
         await kibanaHttpClient.SetDarkModeAsync(false, null, cancellationToken);
+
+        var space = new KibanaSpace()
+        {
+            Id = "default",
+            Name = "Krowa",
+            Initials = "D",
+            Color = "#FFDD00",
+            Description = "Hello world",
+            DisabledFeatures = []
+        };
+
+
+        logger.LogInformation($"Getting default space");
+        //space = await kibanaHttpClient.GetSpaceAsync("default", cancellationToken);
+
+        logger.LogInformation("Space: {@defaultSpace}", space);
+
+
+
+
+        logger.LogInformation("Updating default Kibana space {@space}", space);
+        var response = await kibanaHttpClient.UpdateSpaceAsync(space, cancellationToken);
+
+        logger.LogInformation("Space update: {status}", response.StatusCode);
+
         
         
     }
